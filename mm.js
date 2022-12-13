b584eeb342c5bee71ae4dd9d94127ecb16333fc3
toothless13 <50870607+toothless13@users.noreply.github.com>
2022-12-13 16:52:36 +0000

    Add all filled arrays

--- mm.js
+++ mm.js
@@ -1,5 +1,13 @@
-const adj1 = ['Debonair', 'Enthusiastic', 'Hollow', 'Outrageous', 'Shaggy', 'Little'];
-const name1 = ['Jacob', 'Alfie', 'Ibraheem', 'Jimmy', 'Seth', 'Shawn'];
-const adj2 = ['tired', 'wise', 'wonderful', 'confused', 'mighty', 'voracious'];
-const name2 = ['Penelope', 'Demi', 'Flora', 'Lexi', 'Amelia', 'Claudia'];
-const placeMet = ['the shops', 'the pub', 'the coffee shop', 'the aquarium', 'the zoo', 'the toilets'];
+const adj1Arr = ['Debonair', 'Enthusiastic', 'Hollow', 'Outrageous', 'Shaggy', 'Little'];
+const name1Arr = ['Jacob', 'Alfie', 'Ibraheem', 'Jimmy', 'Seth', 'Shawn'];
+const adj2Arr = ['tired', 'wise', 'wonderful', 'confused', 'mighty', 'voracious'];
+const name2Arr = ['Penelope', 'Demi', 'Flora', 'Lexi', 'Amelia', 'Claudia'];
+const placeMetArr = ['the shops', 'the pub', 'the coffee shop', 'the aquarium', 'the zoo', 'the toilets'];
+const activityArr = ['play dodgeball', 'watch a movie', 'throw some shapes', 'make pottery bowls', 'build lego', 'smash avocados'];
+const heWoreArr = ['bright red shoes', 'a leather vest', 'fish shaped gloves', 'a very large hat', 'fishnets', 'a tuxedo'];
+const sheWoreArr = ['a sparkly skirt', 'platform boots', 'a stylish wooly jumper', 'a baseball cap', 'a mask', 'a wristwatch'];
+const heDidArr = ['went bike riding', 'drank some Baileys', 'played guitar', 'watched a film', 'made a cup of tea', 'started a fire'];
+const sheDidArr = ['looked at her phone', 'went horse riding', 'went swimming', 'made a cake', 'played with a puppy', 'hailed a taxi'];
+const consequenceArr = ['the sun came out', 'the pasta boiled over', 'the car crashed into the wall', 'the food burnt to the pan', 'the author finished the book', 'they both won the lottery'];
+const worldSaidArr = ['phew', 'I love it', 'what a time to be alive', "that's awful", "we're so happy", "that's amazing"];
+
