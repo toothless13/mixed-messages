61b15897182a97ee5e3c274478efb9252758a9b7
toothless13 <50870607+toothless13@users.noreply.github.com>
2022-12-13 15:03:22 +0000

    Add first 5 arrays

---
+++ mm.js
@@ -0,0 +1,5 @@
+const adj1 = ['Debonair', 'Enthusiastic', 'Hollow', 'Outrageous', 'Shaggy', 'Little'];
+const name1 = ['Jacob', 'Alfie', 'Ibraheem', 'Jimmy', 'Seth', 'Shawn'];
+const adj2 = ['tired', 'wise', 'wonderful', 'confused', 'mighty', 'voracious'];
+const name2 = ['Penelope', 'Demi', 'Flora', 'Lexi', 'Amelia', 'Claudia'];
+const placeMet = ['the shops', 'the pub', 'the coffee shop', 'the aquarium', 'the zoo', 'the toilets'];
